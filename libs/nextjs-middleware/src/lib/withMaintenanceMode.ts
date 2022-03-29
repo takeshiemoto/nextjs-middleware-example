11e760e7b21f9eb5eb1e48397716f10f7c45465f
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2022-03-30 06:27:07 +0900

    chore: dev

--- libs/nextjs-middleware/src/lib/withMaintenanceMode.ts
+++ libs/nextjs-middleware/src/lib/withMaintenanceMode.ts
@@ -2,10 +2,18 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 
 import { Middleware } from './types';
 
+const Repository = {
+  async getAll() {
+    return Promise.reject('Error');
+  },
+};
+
 export const withMaintenance = (middleware?: Middleware) => {
   return async (req: NextRequest, event: NextFetchEvent) => {
-    if (req.ip === '106.184.63.255') {
-      return new Response('Maintenance');
+    try {
+      await Repository.getAll();
+    } catch (error) {
+      return new Response(String(error));
     }
 
     if (!middleware) {
